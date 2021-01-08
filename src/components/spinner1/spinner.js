b4a9d7fd80ed3c5309071b3c01b9f90bc38b3204
Aigul <aigul.sibgatulli@mail.ru>
2021-01-09 02:15:59 +0300

    spinner

---
+++ src/components/spinner1/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-7zi5dz08xa7">
+            <div className="ldio-7x1bktrn4mh">
+                <div></div><div></div><div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
